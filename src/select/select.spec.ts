4ed1b998870d2cb3d36d826c467b68d02dc35721
Rex YE <rexebin@gmail.com>
2017-01-03 22:13:12 +0000

    no test yet.

--- src/select/select.spec.ts
+++ src/select/select.spec.ts
@@ -2,16 +2,48 @@ import { Select } from './select';
 /**
  * Created by Rex on 03/01/2017.
  */
+type SelectItem = {
+    id: string,
+    value: string
+}
 
 describe('Select', () => {
-    let select: Select;
+    let select: Select<SelectItem>;
+    let items: SelectItem[];
     beforeEach(() => {
-       select = new Select();
+        items = [
+            {
+                id: '1',
+                value: '10'
+            },
+            {
+                id: '2',
+                value: '20'
+            },
+            {
+                id: '3',
+                value: '30'
+            },
+            {
+                id: '4',
+                value: '40'
+            }
+        ];
+        select = new Select(items);
     });
     it('should test', () => {
         expect(true).toBe(true);
     });
-    it('should initialize', () => {
-        expect(select).toBeTruthy();
+    
+    it('should return default item', () => {
+
+    });
+    
+    it('should return true if checked item is selected', () => {
+        
+    });
+    
+    it('should return false if checked item is not selected', () => {
+        
     });
 });
--- src/select/select.ts
+++ src/select/select.ts
@@ -1,9 +1,40 @@
+import { BehaviorSubject, Observable } from 'rxjs';
 /**
  * Created by Rex on 03/01/2017.
  */
 
-export class Select{
-    constructor(){
-        
+export interface ISelectItem {
+    id: number | string
+}
+
+export type Compare<T> = (a: T, b: T) => boolean;
+
+export class Select<T extends ISelectItem> {
+    private _behaviorSubject: BehaviorSubject<T> = new BehaviorSubject<T>(null);
+    private _currentValue: T;
+    private _previousValue: T;
+    public currentValue: Observable<T> = this._behaviorSubject.asObservable();
+    
+    public get previousValue() {
+        return this._previousValue;
     }
+    
+    constructor(private _items: T[], private _defaultSelectedIndex: number = 0) {
+        this._previousValue = this._currentValue = _items[_defaultSelectedIndex];
+        this._behaviorSubject = new BehaviorSubject<T>(this._currentValue);
+    }
+    
+    select(item: T) {
+        this._previousValue = this._currentValue;
+        this._currentValue = item;
+        this._behaviorSubject.next(item);
+    }
+    
+    isSelected(item: T, compare?: Compare<T>): boolean {
+        if (!compare) {
+            return item.id === this._currentValue.id;
+        }
+        return compare(item, this._currentValue);
+    }
+    
 }
